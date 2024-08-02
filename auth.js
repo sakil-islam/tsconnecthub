7e21e8e4c06c9c8e880f857cf174be9e60d0f28b
sakil-islam <sakilislam1080@gmail.com>
2024-08-03 00:13:50 +0600

    #6: Protected route

--- auth.js
+++ auth.js
@@ -4,6 +4,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { User } from "./model/user-model";
 
 import bcrypt from "bcryptjs";
+import { authConfig } from "./auth.config";
 
 export const {
   handlers: { GET, POST },
@@ -11,9 +12,7 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
-  session: {
-    strategy: "jwt",
-  },
+  ...authConfig,
   providers: [
     CredentialsProvider({
       async authorize(credentials) {
