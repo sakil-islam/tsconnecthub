b41dc93ae3c37970540cead79d4e80fa55aa1860
sakil-islam <sakilislam1080@gmail.com>
2024-08-05 13:46:32 +0600

    #11: Database Design restructure, Course Details, Calculations

--- queries/categories.js
+++ queries/categories.js
@@ -1,7 +1,23 @@
 import { Category } from "@/model/category-model";
-import { replaceMongoIdInArray } from "@/lib/convertData";
+import {
+  replaceMongoIdInArray,
+  replaceMongoIdInObject,
+} from "@/lib/convertData";
 
 export async function getCategories() {
-  const categories = await Category.find({}).lean();
-  return replaceMongoIdInArray(categories);
+  try {
+    const categories = await Category.find({}).lean();
+    return replaceMongoIdInArray(categories);
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export async function getCategoryDetails(categoryId) {
+  try {
+    const category = await Category.findById(categoryId).lean();
+    return replaceMongoIdInObject(category);
+  } catch (error) {
+    throw new Error(error);
+  }
 }
