337cf7ebf51cb72677a0deb5fce4899681e94472
sakil-islam <sakilislam1080@gmail.com>
2024-08-11 23:34:40 +0600

    #19: Course progress and Bugfix
    track course progress

--- app/(main)/courses/[id]/_components/CourseCurriculam.jsx
+++ app/(main)/courses/[id]/_components/CourseCurriculam.jsx
@@ -1,4 +1,6 @@
-import { Accordion } from "@/components/ui/accordion";
+import {
+  Accordion,
+} from "@/components/ui/accordion";
 
 import { BookCheck } from "lucide-react";
 import { Clock10 } from "lucide-react";
@@ -6,19 +8,28 @@ import { Clock10 } from "lucide-react";
 import CourseModuleList from "./module/CourseModuleList";
 
 const CourseCurriculam = ({ course }) => {
-  const totalDuration = course?.modules.reduce(function (acc, obj) {
-    return acc + obj.duration;
-  }, 0);
+  const totalDuration = course?.modules
+    .map((item) => {
+      return item.lessonIds.reduce(function (acc, obj) {
+        return acc + obj.duration;
+      }, 0);
+    })
+    .reduce(function (acc, obj) {
+      return acc + obj;
+    }, 0);
+
+  console.log({ totalDuration });
+
   return (
     <>
-      <div className="flex gap-x-5 items-center justify-center flex-wrap mt-4 mb-6 text-gray-600 text-sm">
+      <div class="flex gap-x-5 items-center justify-center flex-wrap mt-4 mb-6 text-gray-600 text-sm">
         <span className="flex items-center gap-1.5">
           <BookCheck className="w-4 h-4" />
           {course?.modules?.length} Chapters
         </span>
         <span className="flex items-center gap-1.5">
           <Clock10 className="w-4 h-4" />
-          {(totalDuration / 60).toPrecision(2)} Hours
+          {(totalDuration / 3600).toPrecision(2)} Hours
         </span>
       </div>
 
@@ -30,9 +41,7 @@ const CourseCurriculam = ({ course }) => {
         className="w-full"
       >
         {course?.modules &&
-          course?.modules.map((module) => {
-            return <CourseModuleList module={module} key={module?._id} />;
-          })}
+          course?.modules.map((module) => <CourseModuleList module={module} />)}
       </Accordion>
     </>
   );
