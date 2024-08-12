a54b5189c1ceabb40b55f587291072e2f38d76cb
sakil-islam <sakilislam1080@gmail.com>
2024-08-12 23:24:12 +0600

    #21: Lesson side bar actual data, Lesson states maintained, ⁠Lesson unique URL
    lesson sidebar with actual data, maintaining lesson states, and generating unique URLs for each lesson.

--- app/(main)/courses/[id]/lesson/_components/course-sidebar.jsx
+++ app/(main)/courses/[id]/lesson/_components/course-sidebar.jsx
@@ -1,11 +1,42 @@
-"use client";
 import { CourseProgress } from "@/components/course-progress";
 
 import { GiveReview } from "./give-review";
 import { DownloadCertificate } from "./download-certificate";
 import { SidebarModules } from "./sidebar-modules";
+import { getCourseDetails } from "@/queries/courses";
+import { getLoggedInUser } from "@/lib/loggedin-user";
+import { Watch } from "@/model/watch-model";
 
-export const CourseSidebar = () => {
+export const CourseSidebar = async ({ courseId }) => {
+  const course = await getCourseDetails(courseId);
+  const loggedinUser = await getLoggedInUser();
+
+  const updatedModules = await Promise.all(
+    course?.modules.map(async (module) => {
+      const moduleId = module._id.toString();
+      const lessons = module?.lessonIds;
+
+      const updatedLessons = await Promise.all(
+        lessons.map(async (lesson) => {
+          const lessonId = lesson._id.toString();
+          const watch = await Watch.findOne({
+            lesson: lessonId,
+            module: moduleId,
+            user: loggedinUser.id,
+          }).lean();
+          if (watch?.state === "completed") {
+            console.log(`This lesson ${lesson.title} has completed`);
+            lesson.state = "completed";
+          }
+          return lesson;
+        })
+      );
+      return module;
+    })
+  );
+
+  console.log(updatedModules);
+
   return (
     <>
       <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
@@ -16,11 +47,11 @@ export const CourseSidebar = () => {
           </div>
         </div>
 
-        <SidebarModules />
+        <SidebarModules courseId={courseId} modules={updatedModules} />
 
         <div className="w-full px-6">
-          <DownloadCertificate />
-          <GiveReview />
+          <DownloadCertificate courseId={courseId} />
+          <GiveReview courseId={courseId} />
         </div>
       </div>
     </>
